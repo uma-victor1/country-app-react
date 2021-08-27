73cf547e5dd4bfcf88c28ab3868c888127f90d37
Uma Victor <umavictor11@gmail.com>
2021-08-27 23:47:19 +0100

    adding country details in details page

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -26,6 +26,22 @@ function Details() {
                     <i class="fa fa-arrow-left"></i> Back
                     </button>
                 </div>
+                <div className="container flex mx-auto p-8 pl-0 pr-0">
+                    <img src={state.flag} className="w-1/2 pr-8" alt={state.name}></img>
+                    <div className="p-8 pl-0">
+                        <h2 className="font-bold text-2xl mb-8"></h2>
+                        <div className="grid grid-cols-2 gap-x-20 gap-y-4">
+                        <p>Native Name: <span className="dark:text-gray-400 text-gray-700 text-sm">{state.nativeName}</span></p>
+                        <p>Population: <span className="dark:text-gray-400 text-gray-700 text-sm">{state.population}</span></p>
+                        <p>Region: <span className="dark:text-gray-400 text-gray-700 text-sm">{state.region}</span></p>
+                        <p>Sub Region: <span className="dark:text-gray-400 text-gray-700 text-sm">{state.subregion}</span></p>
+                        <p>Capital: <span className="dark:text-gray-400 text-gray-700 text-sm">{state.capital}</span></p>
+                        <p>Top Level Domain: <span className="dark:text-gray-400 text-gray-700 text-sm">{state.topLevelDomain[0]}</span></p>
+                        <p>Currencies: <span className="dark:text-gray-400 text-gray-700 text-sm">{state.currencies.map(cur => cur.name)}</span></p>
+                        <p>Languages: <span className="dark:text-gray-400 text-gray-700 text-sm">{state.languages.map(lang => lang.name+', ')}</span></p>
+                        </div>
+                    </div>
+                </div>
         </div>
     )
 }
