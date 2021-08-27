90f38bc50dfb4ea7a4eb012b2fb9f7b61e0f7ad0
Uma Victor <umavictor11@gmail.com>
2021-08-27 23:39:45 +0100

    building detail page

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { useHistory, useLocation } from 'react-router-dom'
 
 function Details() {
+    const [mode, setMode] = useState(true)
+    const [toggleBtn, setToggleBtn] = useState('<i class="far fa-sun"></i> light Mode')
+
+    let (state) = useLocation()
+    let history = useHistory()
+
+    const goHome = ()=> {
+        history.push("/")
+    }
     return (
-        <div>
-            <p>Loaded from Details</p>
+        <div className="bg-gray-100 dark:bg-gray-800 dark:text-white">
+            <div className="flex container mx-auto">
+                    <h1 className="font-bold text-xl">Where in the World?</h1>
+                    <div className="ml-auto font-medium">
+                        <button onClick={()=> toggleDarkMode()} dangerouslySetInnerHTML={{__html: toggleBtn}}></button>
+                    </div>
+                </div>
+                <div className="container mx-auto mb-16">
+                    <button className="px-8 py-2 bg-white text-gray-600 shadow-md rounded-lg dark:bg-gray-700 dark:text-white"
+                    onClick={()=> {goHome()}}
+                    >
+                    <i class="fa fa-arrow-left"></i> Back
+                    </button>
+                </div>
         </div>
     )
 }
